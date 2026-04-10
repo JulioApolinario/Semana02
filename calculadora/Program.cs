52be2d4511c5480797b812ac82660bda4b8d40d0
Julio Apolinario <152618294+JulioApolinario@users.noreply.github.com>
2026-04-10 14:25:56 +0000

    Criação da API

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -9,22 +9,21 @@ app.MapGet("/", () => {
 
 app.MapGet("/saudacao", () => new { mensagem = "Olá, mundo!" });
 
-app.MapGet("/calcula/{opcao}/{valor1}/{valor2}", (int opcao, int valor1, int valor2) => {
-    switch(opcao) {
-        case 1:
-            return new { mensagem = "Soma.....", resultado = valor1 + valor2 };
-        case 2:
-            return new { mensagem = "Subtração.....", resultado = valor1 - valor2 };
-        case 3: 
-            return new { mensagem = "Multiplicação.....", resultado = valor1 * valor2 };
-        case 4:
-            if (valor2 == 0) {
-                return new { mensagem = "Deu ruim favela, não pode dividir por zero!", resultado = 0 };
-            }
-            return new { mensagem = "Dividindo papi.....", resultado = valor1 / valor2 };
-        default:
-            return new { mensagem = "deu ruim tropa.....", resultado = 0 };
-    }
+
+app.MapGet("/calcula/soma/{a:double}/{b:double}", (double a, double b) => {
+    return Results.Ok(new { mensagem = "valor da soma...", resultado = a + b });
+});
+
+app.MapGet("/calcula/subtracao/{a:double}/{b:double}", (double a, double b) => {
+    return Results.Ok(new { mensagem = "valor da subtracao...", resultado = a - b });
+});
+
+app.MapGet("/calcula/multiplicacao/{a:double}/{b:double}", (double a, double b) => {
+    return Results.Ok(new { mensagem = "valor da multiplicacao...", resultado = a * b });
+});
+
+app.MapGet("/calcula/divisao/{a:double}/{b:double}", (double a, double b) => {
+    return Results.Ok(new { mensagem = "valor da divisao...", resultado = a / b });
 });
 
 app.Run();
--- calculadora/obj/Debug/net10.0/calculadora.AssemblyInfo.cs
+++ calculadora/obj/Debug/net10.0/calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95400d3bdbef0476fbb016390271ea70ff2638a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28fcc50d71e53bb3af932a483bd012b2e37efc6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
